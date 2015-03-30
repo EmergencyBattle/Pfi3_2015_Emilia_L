46c9062d8ae29985b0719729aba798c509eb0c68
EmergencyBattle <emergencybattle@gmail.com>
2015-03-30 17:56:10 +0200

    Quote App possibly done

---
+++ quoteapp/src/main/java/com/example/emilia/quoteapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.emilia.quoteapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Random rand = new Random();
+    int n = 0;
+
+    public void buttonOnClick (View v){
+        TextView t1 = (TextView) findViewById(R.id.quote_field);
+        TextView t2 = (TextView) findViewById(R.id.textView);
+
+        n = rand.nextInt(3);
+
+        String[] quote_array = getResources().getStringArray(R.array.quote_array);
+
+        t1.setText(quote_array[n]);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
