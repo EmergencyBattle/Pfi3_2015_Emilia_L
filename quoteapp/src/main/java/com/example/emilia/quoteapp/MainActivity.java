792767e6d3e9caeb1d8c3b402fa2f37773fe593c
EmergencyBattle <emergencybattle@gmail.com>
2015-03-31 21:26:11 +0200

    Assignment done, probably

--- quoteapp/src/main/java/com/example/emilia/quoteapp/MainActivity.java
+++ quoteapp/src/main/java/com/example/emilia/quoteapp/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends ActionBarActivity {
     Random rand = new Random();
     int n = 0;
 
-    Log.i("MyAPP" ,"Button pressed");
 
     public void buttonOnClick (View v){
         TextView t1 = (TextView) findViewById(R.id.quote_field);
@@ -27,13 +26,47 @@ public class MainActivity extends ActionBarActivity {
 
         t1.setText(quote_array[n]);
 
+        Log.i("MyAPP" ,"Button pressed");
 
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MyAPP" ,"Started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MyAPP" ,"Resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MyAPP" ,"Paused");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MyAPP" ,"Destroyed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MyAPP" ,"Stopped");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MyAPP" ,"Created");
     }
 
 
